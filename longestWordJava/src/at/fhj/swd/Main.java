7c852fd80fcb3b5d4356a70be3978c4645f41daa
maxiMac <maximilian.feil@edu.fh-joanneum.at>
2018-10-09 16:00:49 +0200

    made a longest word java project

---
+++ longestWordJava/src/at/fhj/swd/Main.java
@@ -0,0 +1,36 @@
+package at.fhj.swd;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String quotes = "The Scandal of education is that every time " +
+            "    you teach something, you deprive a student of the pleasure" +
+            "    and benefit of discovery." +
+            "    (Seymour Papert, born February 29, 1928 died July 31 2016)" +
+            "    If debugging is the process of removing bugs, then programming" +
+            "    must be the process of putting them in." +
+            "    (Edsger W. Dijkstra)";
+
+        String[] words = quotes.split("\\W+\\s*");
+
+        System.out.println(quotes);
+        System.out.println();
+        System.out.println("=====================");
+        System.out.println();
+        System.out.println(Arrays.toString(words));
+        System.out.println("There are " + words.length + " words in this text");
+
+        String maxChars = "";
+        for (String word : words) {
+            if (maxChars.length() < word.length()){
+                maxChars = word;
+            }
+        }
+
+        System.out.println("The longest word in the text is: " + maxChars);
+
+    }
+}
