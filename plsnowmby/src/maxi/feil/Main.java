0b10cc78229d4807d6e5c35a61efc0e93d6540cb
maxi <maximilian.feil@edu.fh-joanneum.at>
2018-11-30 20:55:24 +0100

    manjaro global gitignore working

--- plsnowmby/src/maxi/feil/Main.java
+++ plsnowmby/src/maxi/feil/Main.java
@@ -3,6 +3,6 @@ package maxi.feil;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello there fellow nub");
+        System.out.println("finally working");
     }
 }
